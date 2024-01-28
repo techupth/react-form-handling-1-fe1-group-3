8f65b91599c05cd5c5607a5dd4990ff866c76045
sirithanatharathorntv <sirithanatharathorntv@gmail.com>
2024-01-28 17:18:25 +0700

    create branch pop

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [startMessage, setMessage] = useState("Greeting Message");
+  const [textInput, setTextInput] = useState("");
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{startMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input id="greeting-message" type="text" value={textInput} onChange={(event) => {
+          setTextInput(event.target.value);
+        }} />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={() => { setMessage(textInput) }}>Update text</button>
       </div>
     </div>
   );
